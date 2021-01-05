78c47437425af46ec8bf5a045f65294eacb95203
LamChieu <52293205+LamChieu@users.noreply.github.com>
2021-01-05 16:34:05 +0700

    chieu

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import "./App.css";
 import AddBookMark from "./components/AddBookMark";
 import BookMarks from "./components/BookMarks";
--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: "http://localhost:7000/",
+  baseURL: "https://chieu-backend.herokuapp.com/",
 });
 
 axiosClient.interceptors.request.use(async (config) => {
